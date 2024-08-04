64aac79c2e2a8bcbf52bfc9c90c29fa6f974d587
0xp17j8ff01 <111459558+0xp17j8@users.noreply.github.com>
2024-08-04 14:20:48 +0100

    rkep.h

---
+++ rkep.h
@@ -0,0 +1,20 @@
+#ifndef _RTKEPROCESS_H_
+#define _RTKEPROCESS_H_
+
+#include <ntddk.h>
+
+#pragma alloc_text(INIT, DriverEntry)
+#pragma alloc_text(PAGE, OnUnload)
+#pragma alloc_text(PAGE, Dispatch)
+
+NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING theRegistryPath);
+VOID OnUnload(IN PDRIVER_OBJECT DriverObject);
+NTSTATUS Dispatch(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS HideProcess();
+
+#define DEVICE_NAME L"\\Device\\RTKEProcess"
+#define DOS_DEVICE_NAME L"\\DosDevices\\RTKEProcess"
+
+PLIST_ENTRY pList;
+
+#endif 
