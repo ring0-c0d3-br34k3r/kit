15d4e268e3e865b6b86d73dd675afcdf090f0f3d
0xp17j8ff01 <111459558+0xp17j8@users.noreply.github.com>
2024-08-06 06:38:42 +0100

    pathme.h

--- pathme.h
+++ pathme.h
@@ -2,16 +2,13 @@
 #define _TRANSME_IM_JUSTan_PATH_ 1
 
 NTSTATUS prcc_Info(HANDLE pid);
+  BOOLEAN PebHouse(PEPROCESS Process, WCHAR* szFullName, WCHAR* szBaseName);
+    BOOLEAN TakeMe(PEPROCESS Process, WCHAR* szFullName);
+      BOOLEAN PairToPair(PEPROCESS Process, WCHAR* ProcessName);
 
-BOOLEAN PebHouse(PEPROCESS Process, WCHAR* szFullName, WCHAR* szBaseName);
+      BOOLEAN MyFace(PEPROCESS Process, char* cName);
+    BOOLEAN PebFace(PEPROCESS Process, WCHAR* szFullName);
+  BOOLEAN Transing(HANDLE pid);
+BOOLEAN W10_imgPOINT(PEPROCESS Process, WCHAR* szFullName);
 
-BOOLEAN PathPebProcessParameters(PEPROCESS Process, WCHAR* szFullName);
-
-BOOLEAN PathSeAuditProcessCreationInfo(PEPROCESS Process, WCHAR* ProcessName);
-
-BOOLEAN PathImageFileName(PEPROCESS Process, char* cName);
-
-BOOLEAN PathSeFileObject(PEPROCESS Process, WCHAR* szFullName);
-
-BOOLEAN Transing(HANDLE pid);
 #endif
